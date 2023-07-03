9f5fde9a82dbdce04ebb949873d1afd97910b5e2
Klenky <92346602+Klenky@users.noreply.github.com>
2023-07-02 23:04:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,24 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
+const generateTarget = () => { return Math.floor(Math.random() * 10)};
+
+const compareGuesses = (userGuess, computerGuess, targetNum) => {
+  const userDiff = Math.abs(targetNum - userGuess);
+  const computerDiff = Math.abs(targetNum - computerGuess);
+  if(userDiff <= computerDiff){
+    return true;
+  }else {return false;}
+}
+
+const updateScore = winner => {
+  if(winner === 'human'){
+    humanScore += 1;
+  }else {computerScore++;}
+}
+
+const advanceRound = () => {currentRoundNumber++;}
+
+console.log(compareGuesses(2, 1, 3));
+console.log(humanScore);
 
